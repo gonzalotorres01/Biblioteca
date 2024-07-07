51b6243a08dc9caf754fd8e945b2237554d7d1b6
gonzalo <lucho31109@gmail.com>
2024-07-06 21:42:22 -0300

    seguidos con javaScript

--- JavaScript_/app.js
+++ JavaScript_/app.js
@@ -91,15 +91,15 @@
 
 
 // // // **** PARAMETROS DE LAS FUNCIONES****
-// // function imprimir(nombre, apellido) {
+// function imprimir(nombre, apellido) {
 
-// //     apellido = apellido || "el apellido qliao";
+//   apellido = apellido || "el apellido qliao";
 
-// //     // if (apellido === undefined) {
-// //     //     apellido = "falta el apellido";
-// //     // }
-// //     console.log(nombre + " " + apellido);
-// // };
+// if (apellido === undefined) {
+//     apellido = "falta el apellido";
+//  }
+//    console.log(nombre + " " + apellido);
+//  };
 
 // // // variable anonima
 // // imprimir("gonza", "torres");
@@ -110,28 +110,28 @@
 
 // // function imprimir1(nombres) {
 // //     console.log(nombres);
-// //     // console.log(persona.nombre1 + " " + persona.apellido);
-// // }
+//     // console.log(persona.nombre1 + " " + persona.apellido);
+// }
 
-// // imprimir1({ 
+ // imprimir1({ 
 
-// //     nombre1: "gonza",
-// //     apellido: "tores"
-// // });
-
-
-// // var obj = {
-// //     nombre: "gonzalito",
-// //     apellido: "pari"
-// // }
-
-// // imprimir (obj)
-// // console.log(obj);
+ //     nombre1: "gonza",
+ //     apellido: "tores"
+ // });
 
 
-// // function im(params) {
+ // var obj = {
+ //     nombre: "gonzalito",
+ //     apellido: "pari"
+ // }
+
+ // imprimir (obj)
+ // console.log(obj);
+
+
+ // function im(params) {
     
-// // }
+ // }
 
 
 // // retorno de funcion
@@ -169,78 +169,139 @@
 // }
 
 
+// desbloc
+
+// function crearPersona(nombre, apellido) {
+//     return{
+//         nombre:nombre,
+//         apellido:apellido
+//     }
+// }
+
+// var persona= crearPersona("gonzalo","torres");
 
 
-function crearPersona(nombre, apellido) {
-    return{
-        nombre:nombre,
-        apellido:apellido
-    }
+// console.log(persona);
+// console.log(persona.nombre);
+// console.log(persona.apellido);
+
+
+// function creaFuncion() {
+//     return function (nombre) {
+//         console.log("me creo " + nombre);
+//         return function(){
+//             console.log("segunda funcion");
+//         }
+//     }
+// }
+
+// var nuevaFuncion= creaFuncion();
+// // nuevaFuncion();
+// // nuevaFuncion(persona.nombre)
+// // console.log(nuevaFuncion); 
+// // console.log(nuevaFuncion());
+// var segundaFuncion= nuevaFuncion (persona.apellido);
+
+// segundaFuncion();
+
+
+
+
+
+
+// // FUNCIONES DE PRIMERA CLASE ///////////////
+
+
+//  las funciones son objetos
+
+// function a() {
+//     console.log(" funcion a");
+// }
+
+// a();
+
+// a.name= "gonzalito"
+
+// console.log(a.name );
+
+
+// function b(){
+//     console.log("esta es otra funcion de prueba");
+// }
+// b();
+
+// b.apply = "torres"
+
+// console.log(b.apply);
+
+
+// a las funciones les podemos meter metodos
+
+
+
+// a.direccion= {
+//     pais: "argentina",
+//     provincia: "cordoba",
+//     ciudad: "capital",
+//     domicilio: {
+//         barrio: "pueirredon",
+//         calle:"charcas",
+//         numeracion: "2311",
+//     }
+// }
+
+
+// metodos y el objeto THIS
+
+// var personasx = {
+//     nombre: "gonzalete",
+//     apellido: "parii",
+//     imprimirNombre: function(){
+//         // console.log(this);
+//         console.log(this.nombre +" "+ this.apellido);
+//     },
+//     direccion:{
+//         pais: "argentina",
+//         obtenerPais:function(){
+//             // console.log(this);
+//             var self = this;
+            
+//             var nuevaDireccion = function(){
+//                 console.log(self);
+//                 console.log("la direccion es " + self.pais);
+//             }
+//             nuevaDireccion()
+//         }
+//     }
+// };
+
+// personasx.nombre = "lucho"
+
+// personasx.imprimirNombre();
+// personasx.direccion.obtenerPais();
+
+
+//  la palabra reservada New ////
+
+// var carlos = new personalbar();
+
+// function Persona(nombre,apellido){
+// this.nombre = "gonza";
+// this.apellido = "tores";
+// this.edad= 22;
+// // this.nombreCompleto= function(){
+// this.imprimirPersona=function(){
+//     return this.nombre + " "+ this.apellido + "("+this.edad+")";
+// }
+// console.log("paso por aqui");
 }
 
-var persona= crearPersona("gonzalo","torres");
+// var juan = new Persona();
+
+// console.log(juan);
+// console.log(juan.nombre);
+// console.log(juan.nombreCompleto());
+// var juan = new Persona("gonza","tores");
 
 
-console.log(persona);
-console.log(persona.nombre);
-console.log(persona.apellido);
-
-
-function creaFuncion() {
-    return function (nombre) {
-        console.log("me creo " + nombre);
-        return function(){
-            console.log("segunda funcion");
-        }
-    }
-}
-
-var nuevaFuncion= creaFuncion();
-// nuevaFuncion();
-// nuevaFuncion(persona.nombre)
-// console.log(nuevaFuncion); 
-// console.log(nuevaFuncion());
-var segundaFuncion= nuevaFuncion (persona.apellido);
-
-segundaFuncion();
-
-// FUNCIONES DE PRIMERA CLASE
-// las funciones son objetos
-
-function a() {
-    console.log(" funcion a");
-}
-
-a();
-
-a.nombre= "gonzalito"
-
-console.log(a.nombre );
-
-a.direccion= {
-    pais: "argentina",
-    provincia: "cordoba",
-    ciudad: "capital",
-    domicilio: {
-        barrio: "pueirredon",
-        calle:"charcas",
-        numeracion: "2311",
-    }
-}
-
-
-var personasx = {
-    nombre: "gonzalete",
-    apellido: "parii",
-    imprimirNombre: function(){
-        console.log(this);
-    }
-};
-
-personasx.imprimirNombre();
-
-
-
-
-
-
+// console.log( juan.imprimirPersona());
